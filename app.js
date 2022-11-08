528b458d8b51f0c29e2e3f5aa920665dacbc76fc
Boudjelal Yannick <ndjbouman@gmail.com>
2022-11-08 09:57:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
-const express = require("express");
-
 const port = process.env.APP_PORT ?? 5000;
+
+const express = require("express");
 const app = express();
+app.use(express.json());
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -14,9 +15,13 @@ const movieHandlers = require("./movieHandlers");
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 
+app.post("/api/movies", movieHandlers.postMovie);
+
 const usersHandlers = require("./usersHandlers");
 app.get("/api/users", usersHandlers.getUsers);
 app.get("/api/users/:id", usersHandlers.getUsersById);
+
+app.post("/api/users", usersHandlers.postUsers);
 
 app.listen(port, (err) => {
   if (err) {
